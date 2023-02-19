053281bf847984c15bd2ca663d666ed08369bc89
Chaya Stern <chayatwinstern@gmail.com>
2023-02-19 10:20:12 -0500

    commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("N\tN^2\tN^3\tN^2+N^3");
+int n = 10;
+while (n != 60) 
+{
+    double n2 = Math.Pow(n, 2);
+    double n3 = Math.Pow(n, 3);
+    double n4 = n2 + n3;
+    Console.WriteLine($"{n}\t{n2}\t{n3}\t{n4}");
+    n += 10;
+
+} 
+Console.WriteLine("Press any key to continue...");
+Console.ReadLine();
+
